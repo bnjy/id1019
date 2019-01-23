d1d71cc9924d7ec989bf00f3cce616ac92bab2a7
bnjy <benrot@kth.se>
2019-01-23 13:47:21 +0100

    intro work

--- introduction/recursion.exs
+++ introduction/recursion.exs
@@ -1,4 +1,12 @@
 defmodule Recursion do
+  def product(m,n) do
+    if m == 0 do
+      0
+    else
+      product(m-1,n) + n
+    end
+  end
+
   def product_clauses(0, _) do
     0
   end
@@ -23,5 +31,28 @@ defmodule Recursion do
       true ->
         product_cond(m-1, n) + n
       end
+  end
+
+  def exp(_, 0) do 1 end
+  def exp(x,n) do
+    x * exp(x,n-1)
+  end
+
+  def exp_cond(x,n) do
+    cond do
+      n == 0 ->
+        1
+      true ->
+        x * exp_cond(x,n-1)
+      end
+    end
+
+  def exp_case(x,n) do
+    case n do
+      0 ->
+        1
+      _ ->
+      x * exp_case(x,n-1)
+    end
   end
 end
