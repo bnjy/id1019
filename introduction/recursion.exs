a96c4bf043e1f5c5c193d2023eb073ad4149b947
bnjy <benrot@kth.se>
2019-01-23 13:56:11 +0100

    intro work

--- introduction/recursion.exs
+++ introduction/recursion.exs
@@ -1,4 +1,6 @@
 defmodule Recursion do
+
+#simple function with if else condition
   def product(m,n) do
     if m == 0 do
       0
@@ -7,14 +9,7 @@ defmodule Recursion do
     end
   end
 
-  def product_clauses(0, _) do
-    0
-  end
-
-  def product_clauses(m, n) do
-    product_clauses(m-1,n) + n
-  end
-
+#case expression
   def product_case(m, n) do
     case m do
       0 ->
@@ -24,6 +19,7 @@ defmodule Recursion do
     end
   end
 
+#cond-do expression
   def product_cond(m, n) do
     cond do
       m == 0 ->
@@ -31,6 +27,15 @@ defmodule Recursion do
       true ->
         product_cond(m-1, n) + n
       end
+  end
+
+#clauses example
+  def product_clauses(0, _) do
+    0
+  end
+
+  def product_clauses(m, n) do
+    product_clauses(m-1,n) + n
   end
 
   def exp(_, 0) do 1 end
